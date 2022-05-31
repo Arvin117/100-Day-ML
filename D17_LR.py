588a3dd6aee633a4bcc802da8d994777e1321d63
lvzhijin <1017482160@qq.com>
2022-05-31 21:46:44 +0800

    Default Changelist

--- D17_LR.py
+++ D17_LR.py
@@ -1,12 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import h5py
-import scipy
 import cv2
-import imageio
-from PIL import Image
-from scipy import ndimage
-from lr_utils import load_dataset
+
+
+def load_dataset():
+    train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:])  # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:])  # your train set labels
+
+    test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:])  # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:])  # your test set labels
+
+    classes = np.array(test_dataset["list_classes"][:])  # the list of classes
+
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
 
 
 def data():
@@ -174,10 +186,11 @@ def test(d, classes, path):
     my_image = cv2.resize(img, (64, 64)).reshape((1, 64 * 64 * 3)).T
     my_predicted_image = predict(d["w"], d["b"], my_image)
 
-    plt.imshow(img)
+    cv2.imshow("Image", img)
     print("y = " + str(np.squeeze(my_predicted_image)) + ", your algorithm predicts a \"" + classes[
         int(np.squeeze(my_predicted_image)),].decode("utf-8") + "\" picture.")
-    plt.show()
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
