10c5a0c1ed8cbcbc4a7a1cc793e085c36d209c3b
lvzhijin <1017482160@qq.com>
2022-08-29 22:01:39 +0800

    Default Changelist

--- D23_AdaBoost.py
+++ D23_AdaBoost.py
@@ -1,11 +1,17 @@
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.datasets import make_classification
+from sklearn.svm import SVC
 
 X, y = make_classification(n_samples=1000, n_features=4, n_informative=2, n_redundant=0,\
                            random_state=0, shuffle=False)
 
-clf = AdaBoostClassifier(n_estimators=100, random_state=0)
+# clf = AdaBoostClassifier(n_estimators=100, random_state=0)    # 默认准确率0.983
+cl = SVC()  # 初始化个自己的弱学习器, 准确率为0.504
+clf = AdaBoostClassifier(base_estimator=cl, n_estimators=100, random_state=0, algorithm='SAMME')
 clf.fit(X, y)
 
+cl.fit(X, y)
+print(cl.score(X, y))  # 单独用SVM准确率为0.958
+# 这代表多个准确率高的分类器集成之后的效果可能会更差
 clf.predict([[0, 0, 0, 0]])
-clf.score(X, y)
+print(clf.score(X, y))
